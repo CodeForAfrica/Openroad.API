ad134e585febd2b37ffefc3a6e92a3cf3774fab4
Timothy Anthony <anthony.timothy90@gmail.com>
2018-06-08 15:11:58 +0300

    Changing celery redis broker url

--- openroad/settings.py
+++ openroad/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://0.0.0.0:6379/0'
+CELERY_RESULT_BACKEND = 'redis://0.0.0.0:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
